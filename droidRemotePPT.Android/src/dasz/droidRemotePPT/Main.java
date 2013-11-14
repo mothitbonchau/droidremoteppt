3909c9002a725dd23aca4db00149c9f6ac173b69
arthur.zaczek@gmail.com <arthur.zaczek@gmail.com@0307cfd7-a990-e557-c5aa-511187426f5a>
2013-11-14 18:01:17 +0000

    Cleanup UI
    git-svn-id: https://droidremoteppt.googlecode.com/svn/trunk@9 0307cfd7-a990-e557-c5aa-511187426f5a

--- droidRemotePPT.Android/src/dasz/droidRemotePPT/Main.java
+++ droidRemotePPT.Android/src/dasz/droidRemotePPT/Main.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -39,6 +40,7 @@ public class Main extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main);
 		
 		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
