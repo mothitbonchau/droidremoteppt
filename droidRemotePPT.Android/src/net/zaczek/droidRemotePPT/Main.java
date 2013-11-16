18f3f2b675ab358bd01609a00bb5d30d24cc95ba
arthur.zaczek@gmail.com <arthur.zaczek@gmail.com@0307cfd7-a990-e557-c5aa-511187426f5a>
2013-11-16 21:18:10 +0000

    new version 1.2
    git-svn-id: https://droidremoteppt.googlecode.com/svn/trunk@34 0307cfd7-a990-e557-c5aa-511187426f5a

--- droidRemotePPT.Android/src/net/zaczek/droidRemotePPT/Main.java
+++ droidRemotePPT.Android/src/net/zaczek/droidRemotePPT/Main.java
@@ -16,7 +16,6 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
